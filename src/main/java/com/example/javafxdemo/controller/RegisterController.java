e3227acd6bfcd695402ba3aa906cde9bd9fbc503
Subit Timalsina <subit@iimscollege.edu.np>
2023-09-24 10:48:12 +0545

    Selected value from combobox

--- src/main/java/com/example/javafxdemo/controller/RegisterController.java
+++ src/main/java/com/example/javafxdemo/controller/RegisterController.java
@@ -1,10 +1,19 @@
 package com.example.javafxdemo.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 public class RegisterController {
-    public void register(ActionEvent event){
+    @FXML
+    private ComboBox userGender;
+    @FXML
+    private Label resultLabel;
 
+    public void register(ActionEvent event){
+        String selectedGender = (String) userGender.getValue();
+        resultLabel.setText(selectedGender);
     }
 }
